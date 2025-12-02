12a6cc93e7be36c4c6f7b3c5723ac436315fd7a0
Jules Sore-Larregain <Jules.SORE-LARREGAIN@cea.fr>
2025-12-02 14:08:49 +0100

    UPDATE- API complète pour poster

--- src/checkout/checkout.module.ts
+++ src/checkout/checkout.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CheckoutController } from './checkout.controller';
 import { CheckoutService } from './checkout.service';
 import { PaymentsModule } from '../payments/payments.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [PaymentsModule, PrismaModule],
   controllers: [CheckoutController],
   providers: [CheckoutService],
   exports: [CheckoutService],
